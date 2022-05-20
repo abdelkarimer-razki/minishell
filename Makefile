4043ab9ee614aa79c5e41cca578ebf235eddbd90
Abdelkarim Er-razki <aer-razk@e2r9p11.1337.ma>
2022-05-20 16:05:16 +0100

    init 🚀

---
+++ Makefile
@@ -0,0 +1,31 @@
+NAME = minishell
+
+FILES = minishell.c
+
+INCLUDE = minishell.h
+
+CC = cc 
+
+CFLAGS = -Wall -Wextra -Werror
+
+OBJS = ${FILES:.c=.o}
+
+all:$(NAME)
+
+$(NAME): $(OBJS) $(INCLUDE)
+	@$(CC) $(CFLAGS) $(OBJS) -I $(INCLUDE) -o $(NAME) -L/usr/local/lib -I/usr/local/include -lreadline
+	@echo "SUUUUUUUUIIIIIII"
+
+%.o : %.c $(INCLUDE)
+	gcc  $(CFLAGS) -o $@ -c $<
+
+clean:
+	@rm -f ${OBJS}
+	@echo "SUUUUUUUUIIIIIII"
+
+fclean: clean
+	@rm -f ${NAME}
+	@rm -f fdf_bonus
+	@echo "SUUUUUUUUIIIIIII"
+
+re: fclean all
---
+++ minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aer-razk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 11:57:54 by aer-razk          #+#    #+#             */
+/*   Updated: 2022/05/20 11:57:56 by aer-razk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
